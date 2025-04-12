0393d71eeeb56948fbf7f20178d13f0d69044bd2
Paul Seibel <paul.seibel@mailbox.org>
2025-04-12 12:23:55 +0200

    humble beginnings

---
+++ README.md
@@ -0,0 +1,14 @@
+# take-note
+
+An experimental automatic note-taking app built upon deno, vite and react.
+
+
+## Development
+
+Start the development server:
+
+```bash
+deno task dev
+```
+
+Visit `http://localhost:5173` to see the app.
---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import deno from "@deno/vite-plugin";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    deno(),
+    react(),
+  ],
+  server: {
+    host: "localhost",
+    port: 3000,
+  },
+});

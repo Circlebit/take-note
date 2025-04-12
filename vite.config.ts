6eb1d02e5636d8e19d71bf4d93ffb9d1e96efb48
Paul Seibel <paul.seibel@mailbox.org>
2025-04-12 16:59:50 +0200

    add tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import deno from "@deno/vite-plugin";
+import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     deno(),
     react(),
+    tailwindcss(),
   ],
   server: {
     host: "localhost",
